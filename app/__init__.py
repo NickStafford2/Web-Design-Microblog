73c5df80465d0a1c9b40aef8f59943617476ddc0
NickStafford2 <41924040+NickStafford2@users.noreply.github.com>
2018-12-10 18:28:55 -0500

    added facelift changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate    import Migrate
 from flask_login      import LoginManager
 from flask_mail       import Mail
+from flask_bootstrap  import Bootstrap
 
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -10,13 +11,15 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 from config           import Config
 import os
 
-app 	= Flask(__name__)
+app 	  = Flask(__name__)
 app.config.from_object(Config)
-db 		= SQLAlchemy(app)
-migrate = Migrate(app, db)
-login   = LoginManager(app)
+db 		  = SQLAlchemy(app)
+migrate   = Migrate(app, db)
+login     = LoginManager(app)
 login.login_view = 'login'
-mail    = Mail(app)
+mail      = Mail(app)
+bootstrap = Bootstrap(app)
+
 
 
 if not app.debug:
