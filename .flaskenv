1d50f365e4e66256496304598da6ece8f57bee31
NickStafford2 <41924040+NickStafford2@users.noreply.github.com>
2018-12-10 15:50:28 -0500

    minor change and added mail requirements

--- .flaskenv
+++ .flaskenv
@@ -1,7 +1,10 @@
 FLASK_APP=microblog.py
 FLASK_DEBUG=1
+MAIL_SERVER=localhost
+MAIL_PORT=8025
+
 #MAIL_SERVER=smtp.googlemail.com
 #MAIL_PORT=587
 #MAIL_USE_TLS=1
-#MAIL_USERNAME=nstaffo2@kent.edu
-#MAIL_PASSWORD=Kent4thewin
+#MAIL_USERNAME=
+#MAIL_PASSWORD=
--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
-from flask import Flask
-from config import Config
+from flask            import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate 	import Migrate
-from flask_login import LoginManager
+from flask_migrate    import Migrate
+from flask_login      import LoginManager
+from flask_mail       import Mail
+
 import logging
-from logging.handlers import SMTPHandler
-from logging.handlers import RotatingFileHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
+from config           import Config
 import os
 
 app 	= Flask(__name__)
@@ -14,6 +16,7 @@ db 		= SQLAlchemy(app)
 migrate = Migrate(app, db)
 login   = LoginManager(app)
 login.login_view = 'login'
+mail    = Mail(app)
 
 
 if not app.debug:
